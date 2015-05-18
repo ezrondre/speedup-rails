85fef268df0ec6f3855d35b975b9c44bf328684a
ezro <ondrej.ezr@easy.cz>
2015-05-18 03:31:32 +0200

    collector tests completed

--- spec/speedup/collectors/collector_spec.rb
+++ spec/speedup/collectors/collector_spec.rb
@@ -6,6 +6,7 @@ module Speedup
     describe Collector do
 
       subject { Collector.new }
+      let(:event) { ActiveSupport::Notifications::Event.new('dummyevt', Time.now, Time.now + 1.second, SecureRandom.hex, {any: :data}) }
 
       context '#initialize' do
         it 'calls #parse_options' do
@@ -47,6 +48,33 @@ module Speedup
           subscriber = subject.register('.notification-test')
           ActiveSupport::Notifications.instrument('.notification-test')
           ActiveSupport::Notifications.unsubscribe(subscriber)
+        end
+
+        it 'does not store_event if registered event is filtered out' do
+          expect(subject).to receive(:filter_event?).and_return(true)
+          expect(subject).to_not receive(:store_event)
+
+          subscriber = subject.register('.notification-test')
+          ActiveSupport::Notifications.instrument('.notification-test')
+          ActiveSupport::Notifications.unsubscribe(subscriber)
+        end
+      end
+
+      context '#store_event' do
+
+        it 'store_event to the request with right_key' do
+          request = double('request')
+          expect(Speedup).to receive(:request).and_return(request)
+          expect(subject).to receive(:key).and_return(:right_key)
+
+          expect(request).to receive(:store_event).with(:right_key, kind_of(Hash))
+          subject.store_event(event)
+        end
+      end
+
+      context '#event_to_data' do
+        it 'returns payload with time and duration' do
+          expect( subject.event_to_data(event) ).to eq( event.payload.merge(time: event.time, duration: event.duration) )
         end
       end
 
