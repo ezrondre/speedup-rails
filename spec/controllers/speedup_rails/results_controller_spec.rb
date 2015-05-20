7d73c53a32518670d821eba550360f3880570936
ezro <ondrej.ezr@easy.cz>
2015-05-20 14:10:00 +0200

    integration test for retrieving data

--- spec/controllers/speedup_rails/results_controller_spec.rb
+++ spec/controllers/speedup_rails/results_controller_spec.rb
@@ -7,12 +7,12 @@ module SpeedupRails
     def initialize_request_data(request_id, adapter=:memory)
       Speedup.adapter = :memory
       Speedup.prepare_collectors
-      data = YAML::load_file(File.expand_path('../../../data/test.yml', __FILE__))
-      Speedup.adapter.write(request_id, Speedup::RequestData.new.load(data[:contexts], data[:data]) )
+      @data = YAML::load_file(File.expand_path('../../../data/test.yml', __FILE__))
+      Speedup.adapter.write(request_id, Speedup::RequestData.new.load(@data[:contexts], @data[:data]) )
     end
 
     context 'with memory storage' do
-      let(:request_id) { SecureRandom.hex(10) }
+      let(:request_id) { SecureRandom.uuid }
 
       before :each do
         initialize_request_data(request_id)
@@ -21,6 +21,9 @@ module SpeedupRails
       it 'renders a bar' do
         get :show, id: request_id
         expect(response).to be_success
+        expect(assigns(:request_id)).to eq(request_id)
+        expect(assigns(:request)).not_to be_nil
+        expect(assigns(:request)).to eq(@data[:data])
       end
 
     end
